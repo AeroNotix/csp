e2d2c5066b2ef341057be14f4b5869f91bd65b30
Aaron France <aaron.l.france@gmail.com>
2018-09-13 16:06:26 +0200

    Properly wake up receiving/sending part
    Also implement a value "handoff" dance. We need to wait until the
    receiving party has actually received the value, then condition-wait
    on them waking us up to tell us they have received the value

--- csp.lisp
+++ csp.lisp
@@ -1,5 +1,8 @@
 (defpackage :csp
-  (:use :cl :bt))
+  (:use :cl :bt)
+  (:export #:send
+           #:recv
+           #:select))
 (in-package :csp)
 
 
@@ -32,8 +35,8 @@
   (define-channel-state-macro with-recv-offer channel-readers))
 
 (defmethod send ((c channel) value &key (block-p t))
-  (let ((when-receiver-available-notify (make-condition-variable)))
-    (with-slots (lock channel-readers wake-channel-writer queue) c
+  (let ((when-receiver-available-notify (make-condition-variable :name "sender")))
+    (with-slots (lock channel-readers queue) c
       (with-recursive-lock-held (lock)
         (with-send-offer c when-receiver-available-notify
           (loop while (null channel-readers)
@@ -45,19 +48,29 @@
             ;; genericize this
             (push value queue)
             (condition-notify reader)
-            (condition-wait when-receiver-available-notify lock)))))))
+            (condition-wait when-receiver-available-notify lock)
+            (condition-notify reader)
+            t))))))
 
 (defmethod recv ((c channel) &key (block-p t))
-  (let ((when-sender-available-notify (make-condition-variable)))
-    (with-slots (lock channel-writers wake-channel-reader queue) c
+  (let ((when-sender-available-notify (make-condition-variable :name "receiever")))
+    (with-slots (lock channel-writers queue) c
       (with-recursive-lock-held (lock)
         (with-recv-offer c when-sender-available-notify
           (loop while (null channel-writers)
              if block-p
              do (bt:condition-wait when-sender-available-notify lock)
              else do (return-from recv nil))
-          (let ((writer (random-removef channel-writers))
-                ;; genericize this
-                (return-value (pop queue)))
+          (let ((writer (random-removef channel-writers)))
             (condition-notify writer)
-            return-value))))))
+            (bt:condition-wait when-sender-available-notify lock)
+            (let ((return-value (pop queue)))
+              (condition-notify writer)
+              return-value)))))))
+
+(defun channels-from-clauses (clauses)
+  (mapcar #'second clauses))
+
+(defmacro select (clauses)
+  (let ((channels (channels-from-clauses clauses)))
+    `(progn ,@channels)))
